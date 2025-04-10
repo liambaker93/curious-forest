2838c785542a91de4869136397f0367007f848d8
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-04-10 15:28:22 +0100

    continued alteration of scripting

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,19 +1,23 @@
-
 // This function will be the first one used when the user opts to start a new game //
 function gameStart() {
       imageHeader.innerHTML = 
     "<img src='assets/images/fantasy-pub.png' alt='A fantasy pub open with people sat drinking and a barmaid serving.'>";
       mainText.innerHTML = "<p>You walk up to a pub and see two tables full of people laughing, sharing stories. One man sits on a stool in front of the bar and watches you wander towards the bar. What do you do?</p>"; 
+      
       buttonShow();
-}
+      
+      buttonA.innerText = "Accept the drink";
+      buttonB.innerText = "Throw the drink in his face";
+      buttonC.innerText = "Turn and leave the pub";
+    };
 
 // This element will display a button for the user to select their option on each card //
 // This function removes the hidden class from the selection buttons
 
 function buttonShow() {
-    document.getElementById("button-a").classList.remove("hidden");
-    document.getElementById("button-b").classList.remove("hidden");
-    document.getElementById("button-c").classList.remove("hidden");
+    buttonA.classList.remove("hidden");
+    buttonB.classList.remove("hidden");
+    buttonC.classList.remove("hidden");
 }
 
 // This section contains the images needed for each kind of level
@@ -24,3 +28,8 @@ const combatImageSources = ["assets/images/combat-card-one"];
 
 const imageHeader = document.getElementById("image-header");
 const mainText = document.getElementById("main-text");
+
+var buttonA = document.getElementById("button-a");
+var buttonB = document.getElementById("button-b");
+var buttonC = document.getElementById("button-c");
+
