387c5ed0de09c39a771d92f558f24f8b617c24a9
liambaker93 <3c2mrjqh@students.codeinstitute.net>
2025-04-10 13:12:54 +0100

    add function to reveal selection buttons when game is started. Add variable for image sources

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,17 +1,21 @@
 
 // This function will be the first one used when the user opts to start a new game //
-function newGame() {
-    document.getElementById("image-header").innerHTML = 
+function gameStart() {
+      document.getElementById("image-header").innerHTML = 
     "<img src='assets/images/fantasy-pub.png' alt='A fantasy pub open with people sat drinking and a barmaid serving.'>";
-    document.getElementById("main-text").innerHTML = "<p>You walk up to a pub and see two tables full of people laughing, sharing stories. One man sits on a stool in front of the bar and watches you wander towards the bar. What do you do?</p>"; // Fixed missing closing tag
-    let buttonA = document.getElementById("button-a");
-
-    // Check if buttonA is not null before trying to modify its class
-    if (buttonA) {
-        buttonA.classList.remove("hidden"); // Remove the "hidden" class if the buttonA exists
-    } else {
-        console.error("Button element with ID 'button-a' not found.");
-    }
-} // line 7
+      document.getElementById("main-text").innerHTML = "<p>You walk up to a pub and see two tables full of people laughing, sharing stories. One man sits on a stool in front of the bar and watches you wander towards the bar. What do you do?</p>"; 
+      buttonShow();
+}
 
 // This element will display a button for the user to select their option on each card //
+// This function removes the hidden class from the selection buttons
+
+function buttonShow() {
+    document.getElementById("button-a").classList.remove("hidden");
+    document.getElementById("button-b").classList.remove("hidden");
+    document.getElementById("button-c").classList.remove("hidden");
+}
+
+// This section contains the images needed for each kind of level
+
+const combatImageSources = ["assets/images/combat-card-one"];
